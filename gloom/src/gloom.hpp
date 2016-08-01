fffd9a9e5f071fac3694f4c2f4103114b7b88de9
Bart van Blokland (LAPPIE) <bartvblokl@gmail.com>
2016-08-01 11:28:02 +0200

    Made window resizable

--- gloom/src/gloom.hpp
+++ gloom/src/gloom.hpp
@@ -13,7 +13,7 @@
 const int mWidth         = 1024;
 const int mHeight        = 768;
 const std::string mTitle = "OpenGL";
-const GLint mResizable   = GL_FALSE;
+const GLint mResizable   = GL_TRUE;
 const int mSamples       = 4;
 
 #endif
